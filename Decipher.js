70665c6bd78cb12f7234bbcce66512d3967a390c
TaigahG <skyb3105@gmail.com>
2022-11-30 13:03:45 +0700

    week 9

--- Decipher.js
+++ Decipher.js
@@ -9,9 +9,9 @@ function digitalDecipher(m, k) {
   return rslt;
 }
 
-f = digitalDecipher([6, 4, 1, 3, 9, 20], 100);
-g = digitalDecipher([20, 12, 18, 30, 21], 1939);
-h = digitalDecipher([14, 30, 11, 1, 20, 17, 18, 18], 1990);
+f = digitalDecipher([20, 12, 18, 30, 21], 1939);
+g = digitalDecipher([14, 30, 11, 1, 20, 17, 18, 18], 1990);
+h = digitalDecipher([6, 4, 1, 3, 9, 20], 100);
 console.log(f);
 console.log(g);
 console.log(h);
